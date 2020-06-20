06bf060e4365d4dc19a7a49c7241a4d479eb259b
Jianfeng He <jianfeng.he@oliverwyman.com>
2020-06-21 00:18:29 +0800

    add ads management

--- admin/src/main.js
+++ admin/src/main.js
@@ -7,6 +7,8 @@ import router from './router'
 
 import http from './http'
 
+import './style.css'
+
 Vue.prototype.$http = http
 
 Vue.use(ElementUI)
---
+++ admin/src/views/AdList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>广告位列表</h1>
+    <el-table :data="items">
+      <el-table-column prop="_id" label="ID" width="220"></el-table-column>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button
+            type="primary"
+            size="small"
+            @click="$router.push(`/ads/edit/${scope.row._id}`)"
+          >编辑</el-button>
+          <el-button type="danger" size="small" @click="remove(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    this.fetch()
+  },
+  methods: {
+    async fetch() {
+      const { data } = await this.$http.get('/rest/ads')
+      this.items = data
+    },
+    async remove(row) {
+      this.$confirm(`是否确定要删除该幻灯片广告  "${row.name}" ?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const { data } = await this.$http.delete(`/rest/ads/${row._id}`)
+        if (data && data.success) {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+          this.fetch()
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
