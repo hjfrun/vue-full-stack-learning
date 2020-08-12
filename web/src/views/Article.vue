5e056f57218860167d3ca0b2feddab50a6575d1a
Jianfeng He <jianfeng.he@oliverwyman.com>
2020-08-12 11:44:41 +0800

    article details

---
+++ web/src/views/Article.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="page-article" v-if="model">
+    <div class="d-flex py-3 px-2 border-bottom">
+      <div style="transform: rotate(270deg);" class="iconfont icon-icon-test1 text-blue"></div>
+      <strong class="flex-1 text-blue pl-2">{{model.title}}</strong>
+      <div class="text-grey fs-xs">2019-06-19</div>
+    </div>
+    <div v-html="model.body" class="px-3 body fs-lg"></div>
+    <div class="px-3 border-top py-3">
+      <div class="d-flex ai-center">
+        <i class="iconfont icon-cc-menu-circle"></i>
+        <strong class="text-blue fs-lg ml-1">相关资讯</strong>
+      </div>
+      <div>
+        <router-link
+          class="pt-1"
+          tag="div"
+          :to="`/articles/${item._id}`"
+          v-for="item in model.related"
+          :key="item._id"
+        >{{item.title}}</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      model: null,
+    }
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get(`articles/${this.id}`)
+      this.model = res.data
+    },
+  },
+  created() {
+    this.fetch()
+  },
+  watch: {
+    id: 'fetch',
+    // id() {
+    //   this.fetch()
+    // },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.page-article {
+  .icon-icon-test1 {
+    font-size: 1.5385rem;
+  }
+  .body {
+    img {
+      width: 100%;
+      height: auto;
+    }
+    iframe {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
